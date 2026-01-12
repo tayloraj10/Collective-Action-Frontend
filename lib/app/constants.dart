f1591a3266ce655de8ce232735b24389db3c90ed
tayloraj10 <taylor.alan.johnson@gmail.com>
2026-01-11 22:52:54 -0500

    Add AppConstants class with mobile breakpoint and helper method for device detection

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+
 // App-wide constants
+
+class AppConstants {
+  // Breakpoints
+  static const double mobileBreakpoint = 600;
+
+  // Helper method to check if device is mobile
+  static bool isMobile(BuildContext context) {
+    return MediaQuery.of(context).size.width < mobileBreakpoint;
+  }
+}
