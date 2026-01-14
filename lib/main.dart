f9cded86c7b420a55104686fd2c187aa8c6cb2bb
tayloraj10 <taylor.alan.johnson@gmail.com>
2026-01-14 01:32:33 -0500

    Add backend health check on app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:collective_action_frontend/app/theme.dart';
 import 'package:collective_action_frontend/app/router.dart';
 import 'package:flutter/material.dart';
@@ -6,11 +7,25 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'firebase_options.dart';
 import 'providers/theme_provider.dart';
+import 'services/api_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   usePathUrlStrategy();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Call backend health check to spin up backend on app start
+  ApiService()
+      .getHealth()
+      .then((value) {
+        log('Health check result:');
+        log(value.toString());
+      })
+      .catchError((error) {
+        log('Health check error:');
+        log(error.toString());
+      });
+
   runApp(const ProviderScope(child: MyApp()));
 }
 
