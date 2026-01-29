66db5130d951a8cc34ce73126e6ada84bd2af656
tayloraj10 <taylor.alan.johnson@gmail.com>
2026-01-28 19:18:38 -0500

    Refactor SummaryCount component to accept a title parameter, allowing dynamic text display based on count and title.

--- lib/screens/dashboard/components/summary_count.dart
+++ lib/screens/dashboard/components/summary_count.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 
 class SummaryCount extends StatelessWidget {
   final int count;
-  const SummaryCount({required this.count, super.key});
+  final String title;
+  const SummaryCount({super.key, required this.count, this.title = 'active'});
 
   @override
   Widget build(BuildContext context) {
-    return Text('$count active', style: Theme.of(context).textTheme.bodySmall);
+    return Text('$count $title', style: Theme.of(context).textTheme.bodySmall);
   }
 }
